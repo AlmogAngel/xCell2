70639d30dcb59f6dd328be636ac6173558c930f5
AlmogAngel <almogangel@gmail.com>
2023-08-03 16:33:03 +0300

    xCell2CleanGenes - select top var genes for sc data

--- R/xCell2CleanGenes.R
+++ R/xCell2CleanGenes.R
@@ -2,12 +2,15 @@
 #'
 #' This function clean and get shared genes between reference and mixture
 #'
+#'@importFrom Seurat CreateSeuratObject FindVariableFeatures VariableFeatures
 #' @param ref Reference gene expression matrix.
 #' @param mix Mixture gene expression matrix.
+#' @param top_var_genes Select top variable genes using Seurat's FindVariableFeatures function (only for scRNA-seq data)
 #' @param gene_groups Gene group to clean c("Rb", "Mrp", "other_Rb", "chrM", "MALAT1", "chrX", "chrY")
+#' @param n_var_genes Number of top variable genes to select  (only for scRNA-seq data)
 #' @return A list of ref and mix with the shared genes after cleaning
 #' @export
-xCell2CleanGenes <- function(ref, mix, gene_groups = c("Rb", "Mrp", "other_Rb", "chrM", "MALAT1", "chrX", "chrY")){
+xCell2CleanGenes <- function(ref, mix, gene_groups = c("Rb", "Mrp", "other_Rb", "chrM", "MALAT1", "chrX", "chrY", top_var_genes = FALSE, n_var_genes = 10000)){
 
   if (all(startsWith(rownames(ref), "ENSG"))) {
     message("Assuming genes are in Ensembl ID...")
@@ -20,9 +23,27 @@ xCell2CleanGenes <- function(ref, mix, gene_groups = c("Rb", "Mrp", "other_Rb", 
   # Remove gene groups
   ngenes_before <- nrow(ref)
   ref <- ref[!rownames(ref) %in% hs.genelist[hs.genelist$gene_group %in% gene_groups, id],]
+
+  if (top_var_genes) {
+
+    genes_names <- rownames(ref)
+    ref.srt <- Seurat::CreateSeuratObject(counts = ref)
+    message(paste0("Selecting top ", n_var_genes, " variable genes - make sure you are using scRNA-Seq reference data!"))
+    ref.srt <- Seurat::FindVariableFeatures(ref.srt, selection.method = "vst", nfeatures = n_var_genes, verbose = FALSE)
+    var_genes <- Seurat::VariableFeatures(ref.srt)
+
+    # Check if Seurat changed genes names
+    if (!all(rownames(var_genes) %in% genes_names)) {
+      # rownames(ref.norm) <- genes_names # Because Seurat change genes names from "_" to "-"
+      errorCondition("Seurat genes name error")
+    }
+
+    ref <- ref[var_genes,]
+  }
+
   ngenes_after <- nrow(ref)
   ngenes_removed <- ngenes_before - ngenes_after
-  message(paste0(ngenes_removed, " removed from reference."))
+  message(paste0(ngenes_removed, " removed from reference in total."))
 
 
   # TODO: add option to choose only protein coding genes
