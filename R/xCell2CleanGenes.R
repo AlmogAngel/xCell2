4a1eaa2619356bed8a0894ea943673bf6ea398d4
AlmogAngel <almogangel@gmail.com>
2023-08-06 13:29:10 +0300

    using only protein coding genes

--- R/xCell2CleanGenes.R
+++ R/xCell2CleanGenes.R
@@ -7,16 +7,17 @@
 #' @param mix Mixture gene expression matrix.
 #' @param top_var_genes Select top variable genes using Seurat's FindVariableFeatures function (only for scRNA-seq data)
 #' @param gene_groups Gene group to clean c("Rb", "Mrp", "other_Rb", "chrM", "MALAT1", "chrX", "chrY")
+#' @param use_protein_coding Boolean - use only protein coding genes?
 #' @param n_var_genes Number of top variable genes to select  (only for scRNA-seq data)
 #' @return A list of ref and mix with the shared genes after cleaning
 #' @export
-xCell2CleanGenes <- function(ref, mix, gene_groups = c("Rb", "Mrp", "other_Rb", "chrM", "MALAT1", "chrX", "chrY"), top_var_genes = FALSE, n_var_genes = 10000){
+xCell2CleanGenes <- function(ref, mix, gene_groups = c("Rb", "Mrp", "other_Rb", "chrM", "MALAT1", "chrX", "chrY"), use_protein_coding = TRUE, top_var_genes = FALSE, n_var_genes = 10000){
 
   if (all(startsWith(rownames(ref), "ENSG"))) {
-    message("Assuming genes are in Ensembl ID.")
+    message("Assuming genes type are Ensembl.")
     id <- 2
   }else{
-    message("Assuming genes are in symbol ID...")
+    message("Assuming genes type are Symbol.")
     id <- 3
   }
 
@@ -24,18 +25,24 @@ xCell2CleanGenes <- function(ref, mix, gene_groups = c("Rb", "Mrp", "other_Rb", 
   ngenes_before <- nrow(ref)
   ref <- ref[!rownames(ref) %in% hs.genelist[hs.genelist$gene_group %in% gene_groups, id],]
 
+  # Use protein coding genes
+  if (use_protein_coding) {
+    pc_genes <- gencode.v22.broad.category[gencode.v22.broad.category$genes_type == "protein_coding", id]
+    ref <- ref[rownames(ref) %in% pc_genes,]
+  }
+
   if (top_var_genes) {
 
+    message(paste0("Selecting top ", n_var_genes, " variable genes - make sure you are using scRNA-Seq reference data!"))
     genes_names <- rownames(ref)
     ref.srt <- Seurat::CreateSeuratObject(counts = ref)
-    message(paste0("Selecting top ", n_var_genes, " variable genes - make sure you are using scRNA-Seq reference data!"))
     ref.srt <- Seurat::FindVariableFeatures(ref.srt, selection.method = "vst", nfeatures = n_var_genes, verbose = FALSE)
     var_genes <- Seurat::VariableFeatures(ref.srt)
 
     # Check if Seurat changed genes names
-    if (!all(rownames(var_genes) %in% genes_names)) {
+    if (!all(var_genes %in% genes_names)) {
       # rownames(ref.norm) <- genes_names # Because Seurat change genes names from "_" to "-"
-      errorCondition("Seurat genes name error")
+      stop("Seurat genes name error")
     }
 
     ref <- ref[var_genes,]
@@ -45,8 +52,6 @@ xCell2CleanGenes <- function(ref, mix, gene_groups = c("Rb", "Mrp", "other_Rb", 
   ngenes_removed <- ngenes_before - ngenes_after
   message(paste0(ngenes_removed, " removed from reference in total."))
 
-
-  # TODO: add option to choose only protein coding genes
 
   shared_genes <- intersect(rownames(ref), rownames(mix))
   message(paste0(length(shared_genes), " genes are shared between reference and mixture after cleaning."))
--- dev_scripts/package_dev.R
+++ dev_scripts/package_dev.R
@@ -4,8 +4,8 @@ library(xCell2)
 
 setwd("/bigdata/almogangel/xCell2/")
 devtools::document()
-devtools::install(pkg = "/bigdata/almogangel/xCell2/")
 devtools::check()
+
 
 ?xCell2Analysis()
 ?xCell2GetLineage()
@@ -16,7 +16,18 @@ data("ts_labels_with_ontology")
 ts_labels <- readRDS("/bigdata/almogangel/super_ref_for_xcell2/ts_labels.rds")
 ts_labels_with_ontology <- ts_labels %>%
   as_tibble()
+
+gencode.v22.broad.category <- read_tsv("/bigdata/almogangel/xCell2/data/gencode.v22.broad.category.txt", col_names = F)
+colnames(gencode.v22.broad.category) <- c("chrm", "start", "end", "ensembl1", "symbol", "strand", "info", "ensembl", "genes_type")
+gencode.v22.broad.category <- gencode.v22.broad.category %>%
+  select(genes_type, ensembl, symbol) %>%
+  as.data.frame()
+
+
 usethis::use_data(ts_labels_with_ontology)
+usethis::use_data(hs.genelist)
+usethis::use_data(gencode.v22.broad.category, overwrite = T)
+
 
 # license
 usethis::use_mit_license()
