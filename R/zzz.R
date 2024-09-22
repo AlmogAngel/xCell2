315d07a5af2d9d87ae928be9c035c8d12bff2bf5
AlmogAngel <almogangel@gmail.com>
2024-09-22 21:42:26 +0300

    .

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,17 @@
 .onLoad <- function(libname, pkgname) {
   if (!requireNamespace("pracma", quietly = TRUE)) {
-    message("Installing 'pracma' from R-Forge...")
-    install.packages("pracma", repos = "http://R-Forge.R-project.org")
+    packageStartupMessage("Installing 'pracma' from R-Forge...")
+    utils::install.packages("pracma", repos = "http://R-Forge.R-project.org")
   }
   
   if (!requireNamespace("quadprog", quietly = TRUE)) {
-    stop("The 'quadprog' package is required but not installed. It should install automatically from CRAN. Please install it manually if this message persists.")
+    stop("The 'quadprog' package is required but not installed. Please install it manually from CRAN.")
   }
 }
 
 .onAttach <- function(libname, pkgname) {
   if (!requireNamespace("pracma", quietly = TRUE)) {
-    message("The 'pracma' package is required and will be installed from R-Forge.")
-    install.packages("pracma", repos = "http://R-Forge.R-project.org")
+    packageStartupMessage("The 'pracma' package is required and will be installed from R-Forge.")
+    utils::install.packages("pracma", repos = "http://R-Forge.R-project.org")
   }
 }
