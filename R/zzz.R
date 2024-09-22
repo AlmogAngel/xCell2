400de00bc208b3e494d69e471890868512f39d53
AlmogAngel <almogangel@gmail.com>
2024-09-22 21:16:39 +0300

    .

--- R/zzz.R
+++ R/zzz.R
@@ -8,3 +8,10 @@
     stop("The 'quadprog' package is required but not installed. It should install automatically from CRAN. Please install it manually if this message persists.")
   }
 }
+
+.onAttach <- function(libname, pkgname) {
+  if (!requireNamespace("pracma", quietly = TRUE)) {
+    message("The 'pracma' package is required and will be installed from R-Forge.")
+    install.packages("pracma", repos = "http://R-Forge.R-project.org")
+  }
+}
