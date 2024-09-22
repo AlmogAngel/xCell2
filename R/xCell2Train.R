719150c32d1f8c95ee654f81fb51122371dcbe6f
AlmogAngel <almogangel@gmail.com>
2024-09-22 09:04:52 +0300

    .

--- R/xCell2Train.R
+++ R/xCell2Train.R
@@ -600,7 +600,6 @@ LearnParams <- function(gepMat, corMat, signatures, depList, topSpillValue, numT
 #' @import tidyr
 #' @import readr
 #' @import BiocParallel
-#' @importFrom GSEABase geneIds setName GeneSet
 #' @importFrom utils data
 #' @importFrom SummarizedExperiment assays colData
 #' @importFrom minpack.lm nlsLM
